fd99f6dbbec6e825f30a823d59b15c0cd73590d3
Uladzislau <vladislav2256@gmail.com>
2023-02-27 21:10:29 +0300

    Added helloworld.py

---
+++ Lab1/main.py
@@ -0,0 +1,11 @@
+from helloworld import hello_world
+from operation import calculation
+
+
+def main():
+    hello_world()
+    print(calculation(5, 3, "sum"))
+
+
+if __name__ == "__main__":
+    main()
